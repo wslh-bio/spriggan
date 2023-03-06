b406231bb7569741705e2f142931b01687572d69
Kelsey Florek <kelseyflorek22@gmail.com>
2023-03-06 16:01:15 -0600

    fixed script error

--- bin/amrfinder_summary.py
+++ bin/amrfinder_summary.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-import os
+import os, sys
 import glob
 import pandas as pd
+
+selected_genes = sys.argv[1]
 
 # get amrfinder output files and set up lists
 files = glob.glob('data/*.amr.tsv')
@@ -40,44 +42,13 @@ for file in files:
     dfs.append(df)
 
     # summarize all results
-    all_ar_df = join_df(df,sample_id,';')#!/usr/bin/env python3
-import os
-import glob
-import pandas as pd
+    all_ar_df = join_df(df,sample_id,';')
+    all_ar_dfs.append(all_ar_df)
 
-# get amrfinder output files and set up lists
-files = glob.glob('data/*.amr.tsv')
-dfs = []
-all_ar_dfs = []
-selected_ar_dfs = []
-
-# function for cleanining up amrfinder output
-def pretty_df(data,sample):
-    data.columns = data.columns.str.replace(' ', '_')
-    data = data.assign(Sample=sample)
-    data = data[['Sample','Gene_symbol','%_Coverage_of_reference_sequence','%_Identity_to_reference_sequence']]
-    pretty_data = data.set_axis(['Sample', 'Gene', 'Coverage', 'Identity'], axis=1, inplace=False)
-    return pretty_data
-
-# function for joining amrfinder results by a delimiter
-def join_df(data,sample,delim):
-    gene = data['Gene'].tolist()
-    gene = delim.join(gene)
-    coverage = data['Coverage'].tolist()
-    coverage = delim.join(map(str, coverage))
-    identity = data['Identity'].tolist()
-    identity = delim.join(map(str, identity))
-    joined_data = [[sample,gene,coverage,identity]]
-    joined_data = pd.DataFrame(joined_data, columns = ['Sample', 'Gene', 'Coverage', 'Identity'])
-    return joined_data
-
-for file in files:
-    # get sample id from file name
-    sample_id = os.path.basename(file).split('.')[0]
-    # read in amrfinder results as data frame
-    df = pd.read_csv(file, header=0, delimiter='\t')
-
-    # clean up data frame
+    # subset data frame by selected genes
+    mask = df['Gene'].str.contains(selected_genes, case=False, na=False)
+    masked_df = df[mask]
+    # check if any select genes were found
     if masked_df.empty:
         masked_df = masked_df.append({'Sample' : sample_id, 'Gene' : 'None', 'Coverage' : 'None','Identity' : 'None'}, ignore_index = True)
     selected_ar_df = join_df(masked_df,sample_id,';')
